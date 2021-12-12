b134fd2e4fc79f49e74e5f55bd325f7629f7963d
Bohdan <kavatsiuk.b.o@gmail.com>
2021-12-12 05:40:35 +0200

    Radar and Glass shader

---
+++ GenesisGameJam2/Assets/Radar/Scripts/Radar.cs
@@ -0,0 +1,29 @@
+﻿/* 
+    ------------------- Code Monkey -------------------
+    
+    Thank you for downloading the Code Monkey Utilities
+    I hope you find them useful in your projects
+    If you have any questions use the contact form
+    Cheers!
+
+               unitycodemonkey.com
+    --------------------------------------------------
+ */
+ 
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Radar : MonoBehaviour {
+
+    private float rotationSpeed;
+
+    private void Start() {
+        rotationSpeed = 180f;
+    }
+
+    private void Update() {
+        transform.eulerAngles -= new Vector3(0, 0, rotationSpeed * Time.deltaTime);
+    }
+
+}

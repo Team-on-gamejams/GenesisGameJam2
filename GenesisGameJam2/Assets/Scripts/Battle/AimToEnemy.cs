72dd8c05737841798a8240a83b43042e1ed252a9
Team-on <timonsol13@gmail.com>
2021-12-12 08:44:11 +0200

    Integrate level 1

--- GenesisGameJam2/Assets/Scripts/Battle/AimToEnemy.cs
+++ GenesisGameJam2/Assets/Scripts/Battle/AimToEnemy.cs
@@ -9,31 +9,36 @@ public class AimToEnemy : MonoBehaviour
 	[SerializeField] Transform targetToAim;
 
 	List<Transform> targets;
+	float wanderTimer = 0;
+	Vector3 target;
 
 	private void Update() {
 		while(targets.Count != 0 && targets[0] == null) {
 			targets.RemoveAt(0);
 		}
 
-		if (targets.Count == 0)
-			return;
-
-		Transform nearestTarget = targets[0];
-		float nearestDists = (nearestTarget.transform.position - targetToAim.position).magnitude;
-		float dist;
-		for (int i = 1; i < targets.Count; ++i) {
-			if (targets[i] == null)
-				continue;
-			dist = (targets[i].transform.position - targetToAim.position).magnitude;
-			if (dist < nearestDists) {
-				nearestTarget = targets[i];
-				nearestDists = dist;
-			}
+		if (targets.Count == 0) {
+			var targetRotation = Quaternion.LookRotation(targetToAim.position - aim.position);
+			aim.rotation = Quaternion.RotateTowards(aim.rotation, targetRotation, 10 * Time.deltaTime);
 		}
+		else{
+			Transform nearestTarget = targets[0];
+			float nearestDists = (nearestTarget.transform.position - targetToAim.position).magnitude;
+			float dist;
+			for (int i = 1; i < targets.Count; ++i) {
+				if (targets[i] == null)
+					continue;
+				dist = (targets[i].transform.position - targetToAim.position).magnitude;
+				if (dist < nearestDists) {
+					nearestTarget = targets[i];
+					nearestDists = dist;
+				}
+			}
 
-		var targetRotation = Quaternion.LookRotation(nearestTarget.position - aim.position);
-		var str = 10 * Time.deltaTime;
-		aim.rotation = Quaternion.RotateTowards(aim.rotation, targetRotation, 60 * Time.deltaTime);
+			var targetRotation = Quaternion.LookRotation(nearestTarget.position - aim.position);
+			aim.rotation = Quaternion.RotateTowards(aim.rotation, targetRotation, 60 * Time.deltaTime);
+		}
+		
 	}
 
 	public void AddTarget(Collider other) {
